ae2a215e89e3b2c763fcba52687eec3a0a2a76c8
Sven Groot <svgroot@microsoft.com>
2023-07-07 11:09:12 -0700

    Clean up operators.

---
+++ src/Ookii.BinarySize/BinarySize.Operators.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ookii;
+
+partial struct BinarySize
+{
+    /// <summary>
+    /// Returns a new <see cref="BinarySize"/> object whose value is the sum of the specified <see cref="BinarySize"/> object and this instance.
+    /// </summary>
+    /// <param name="value">The value to add.</param>
+    /// <returns>A new object that represents the value of this instance plus <paramref name="value"/>.</returns>
+    public static BinarySize Add(BinarySize size1, BinarySize size2) => checked(size1.Value + size2.Value);
+
+    /// <summary>
+    /// Returns a new <see cref="BinarySize"/> object whose value is the difference of the specified <see cref="BinarySize"/> object and this instance.
+    /// </summary>
+    /// <param name="value">The value to subtract.</param>
+    /// <returns>A new object that represents the value of this instance minus <paramref name="value"/>.</returns>
+    public static BinarySize Subtract(BinarySize size1, BinarySize size2) => checked(size1.Value - size2.Value);
+
+    /// <summary>
+    /// Returns a new <see cref="BinarySize"/> object whose value is the product of the specified <see cref="Int64"/> object and this instance.
+    /// </summary>
+    /// <param name="value">The value to multiply by.</param>
+    /// <returns>A new object that represents the value of this instance times <paramref name="value"/>.</returns>
+    public static BinarySize Multiply(BinarySize size1, BinarySize size2) => checked(size1.Value * size2.Value);
+
+    /// <summary>
+    /// Returns a new <see cref="BinarySize"/> object whose value is the division of the specified <see cref="Int64"/> object and this instance.
+    /// </summary>
+    /// <param name="value">The value to divide by.</param>
+    /// <returns>A new object that represents the value of this instance divided by <paramref name="value"/>.</returns>
+    public static BinarySize Divide(BinarySize size1, BinarySize size2) => checked(size1.Value / size2.Value);
+
+    public static BinarySize Remainder(BinarySize size1, BinarySize size2) => checked(size1.Value % size2.Value);
+
+    /// <summary>
+    /// Returns a new <see cref="BinarySize"/> object whose value is the negation of this instance.
+    /// </summary>
+    /// <returns>A new object that represents the negated value of this instance.</returns>
+    public static BinarySize Negate(BinarySize size) => -size.Value;
+
+    /// <summary>
+    /// Determines whether two specified <see cref="BinarySize"/> values are the same.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if the value of <paramref name="left"/> is the same as the value of <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator ==(BinarySize left, BinarySize right) => left.Value == right.Value;
+
+    /// <summary>
+    /// Determines whether two specified <see cref="BinarySize"/> values are different.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if the value of <paramref name="left"/> is different from the value of <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator !=(BinarySize left, BinarySize right) => left.Value != right.Value;
+
+    /// <summary>
+    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is less than another <see cref="BinarySize"/>.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> is less than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator <(BinarySize left, BinarySize right) => left.Value < right.Value;
+
+    /// <summary>
+    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is less than or equal to another <see cref="BinarySize"/>.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> is less than or equal to <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator <=(BinarySize left, BinarySize right) => left.Value <= right.Value;
+
+    /// <summary>
+    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is greater than another <see cref="BinarySize"/>.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> is greater than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator >(BinarySize left, BinarySize right) => left.Value > right.Value;
+
+    /// <summary>
+    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is greater than or equal to another <see cref="BinarySize"/>.
+    /// </summary>
+    /// <param name="left">A <see cref="BinarySize"/>.</param>
+    /// <param name="right">A <see cref="BinarySize"/>.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> is greater than or equal to <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+    public static bool operator >=(BinarySize left, BinarySize right) => left.Value >= right.Value;
+
+    /// <summary>
+    /// Implements the operator +.
+    /// </summary>
+    /// <param name="left">The left operand.</param>
+    /// <param name="right">The right operand.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static BinarySize operator +(BinarySize left, BinarySize right) => left.Value + right.Value;
+
+    /// <summary>
+    /// Implements the operator -.
+    /// </summary>
+    /// <param name="left">The left operand.</param>
+    /// <param name="right">The right operand.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static BinarySize operator -(BinarySize left, BinarySize right) => left.Value - right.Value;
+
+    /// <summary>
+    /// Implements the operator *.
+    /// </summary>
+    /// <param name="left">The left operand.</param>
+    /// <param name="right">The right operand.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static BinarySize operator *(BinarySize left, BinarySize right) => left.Value * right.Value;
+
+    /// <summary>
+    /// Implements the operator /.
+    /// </summary>
+    /// <param name="left">The left operand.</param>
+    /// <param name="right">The right operand.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static BinarySize operator /(BinarySize left, BinarySize right) => left.Value / right.Value;
+
+    public static BinarySize operator %(BinarySize left, BinarySize right) => left.Value % right.Value;
+
+    /// <summary>
+    /// Implements the unary operator -.
+    /// </summary>
+    /// <param name="value">The operand.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static BinarySize operator -(BinarySize value) => -value.Value;
+
+    /// <summary>
+    /// Performs an explicit conversion from <see cref="Ookii.Jumbo.BinarySize"/> to <see cref="System.Int16"/>.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>
+    /// The result of the conversion.
+    /// </returns>
+    public static explicit operator long(BinarySize value) => value.Value;
+
+    /// <summary>
+    /// Performs an implicit conversion from <see cref="System.Int64"/> to <see cref="Ookii.Jumbo.BinarySize"/>.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>
+    /// The result of the conversion.
+    /// </returns>
+    public static implicit operator BinarySize(long value) => new(value);
+}
--- src/Ookii.BinarySize/BinarySize.cs
+++ src/Ookii.BinarySize/BinarySize.cs
@@ -8,7 +8,7 @@ namespace Ookii;
 /// Provides formatting, parsing and scaling for a value using binary units (e.g. MB).
 /// </summary>
 [TypeConverter(typeof(BinarySizeConverter))]
-public readonly struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySize>, IComparable, IFormattable
+public readonly partial struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySize>, IComparable, IFormattable
 #if NET7_0_OR_GREATER
     , ISpanParsable<BinarySize>
 #endif
@@ -41,12 +41,6 @@ public readonly struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySi
     private static readonly long[] _scalingFactors = new[] { Kibi, Mebi, Gibi, Tebi, Pebi };
 
     /// <summary>
-    /// Gets a zero-valued <see cref="BinarySize"/> instance.
-    /// </summary>
-    /// <value>A <see cref="BinarySize"/> instance with <see cref="Value"/> set to zero.</value>
-    public static readonly BinarySize Zero = new();
-
-    /// <summary>
     /// Initializes a new instance of the <see cref="BinarySize"/> structure with the specified
     /// value.
     /// </summary>
@@ -55,6 +49,8 @@ public readonly struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySi
     {
         Value = value;
     }
+
+    public static BinarySize Zero => default;
 
     /// <summary>
     /// Gets the value of this instance, in bytes.
@@ -103,55 +99,6 @@ public readonly struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySi
     /// The value of this instance in whole and fractional petabytes (<see cref="Value"/> / <see cref="Pebi"/>)
     /// </value>
     public double AsPebi => Value / (double)Pebi;
-
-    /// <summary>
-    /// Returns a new <see cref="BinarySize"/> object whose value is the sum of the specified <see cref="BinarySize"/> object and this instance.
-    /// </summary>
-    /// <param name="value">The value to add.</param>
-    /// <returns>A new object that represents the value of this instance plus <paramref name="value"/>.</returns>
-    public BinarySize Add(BinarySize value)
-    {
-        return Value + value.Value;
-    }
-
-    /// <summary>
-    /// Returns a new <see cref="BinarySize"/> object whose value is the difference of the specified <see cref="BinarySize"/> object and this instance.
-    /// </summary>
-    /// <param name="value">The value to subtract.</param>
-    /// <returns>A new object that represents the value of this instance minus <paramref name="value"/>.</returns>
-    public BinarySize Subtract(BinarySize value)
-    {
-        return Value - value.Value;
-    }
-
-    /// <summary>
-    /// Returns a new <see cref="BinarySize"/> object whose value is the product of the specified <see cref="Int64"/> object and this instance.
-    /// </summary>
-    /// <param name="value">The value to multiply by.</param>
-    /// <returns>A new object that represents the value of this instance times <paramref name="value"/>.</returns>
-    public BinarySize Multiply(long value)
-    {
-        return Value * value;
-    }
-
-    /// <summary>
-    /// Returns a new <see cref="BinarySize"/> object whose value is the division of the specified <see cref="Int64"/> object and this instance.
-    /// </summary>
-    /// <param name="value">The value to divide by.</param>
-    /// <returns>A new object that represents the value of this instance divided by <paramref name="value"/>.</returns>
-    public BinarySize Divide(long value)
-    {
-        return Value / value;
-    }
-
-    /// <summary>
-    /// Returns a new <see cref="BinarySize"/> object whose value is the negation of this instance.
-    /// </summary>
-    /// <returns>A new object that represents the negated value of this instance.</returns>
-    public BinarySize Negate()
-    {
-        return -Value;
-    }
 
     public static BinarySize Parse(ReadOnlySpan<char> s, NumberStyles style = NumberStyles.Number, IFormatProvider? provider = null)
     {
@@ -355,158 +302,6 @@ public readonly struct BinarySize : IEquatable<BinarySize>, IComparable<BinarySi
     {
         return Value.GetHashCode();
     }
-
-    /// <summary>
-    /// Determines whether two specified <see cref="BinarySize"/> values are the same.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if the value of <paramref name="left"/> is the same as the value of <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator ==(BinarySize left, BinarySize right)
-    {
-        return left.Value == right.Value;
-    }
-
-    /// <summary>
-    /// Determines whether two specified <see cref="BinarySize"/> values are different.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if the value of <paramref name="left"/> is different from the value of <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator !=(BinarySize left, BinarySize right)
-    {
-        return left.Value != right.Value;
-    }
-
-    /// <summary>
-    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is less than another <see cref="BinarySize"/>.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is less than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator <(BinarySize left, BinarySize right)
-    {
-        return left.Value < right.Value;
-    }
-
-    /// <summary>
-    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is less than or equal to another <see cref="BinarySize"/>.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is less than or equal to <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator <=(BinarySize left, BinarySize right)
-    {
-        return left.Value <= right.Value;
-    }
-
-    /// <summary>
-    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is greater than another <see cref="BinarySize"/>.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is greater than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator >(BinarySize left, BinarySize right)
-    {
-        return left.Value > right.Value;
-    }
-
-    /// <summary>
-    /// Returns a value indicating whether a specified <see cref="BinarySize"/> is greater than or equal to another <see cref="BinarySize"/>.
-    /// </summary>
-    /// <param name="left">A <see cref="BinarySize"/>.</param>
-    /// <param name="right">A <see cref="BinarySize"/>.</param>
-    /// <returns><see langword="true"/> if <paramref name="left"/> is greater than or equal to <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
-    public static bool operator >=(BinarySize left, BinarySize right)
-    {
-        return left.Value >= right.Value;
-    }
-
-    /// <summary>
-    /// Implements the operator +.
-    /// </summary>
-    /// <param name="left">The left operand.</param>
-    /// <param name="right">The right operand.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static BinarySize operator +(BinarySize left, BinarySize right)
-    {
-        return left.Value + right.Value;
-    }
-
-    /// <summary>
-    /// Implements the operator -.
-    /// </summary>
-    /// <param name="left">The left operand.</param>
-    /// <param name="right">The right operand.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static BinarySize operator -(BinarySize left, BinarySize right)
-    {
-        return left.Value - right.Value;
-    }
-
-    /// <summary>
-    /// Implements the operator *.
-    /// </summary>
-    /// <param name="left">The left operand.</param>
-    /// <param name="right">The right operand.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static BinarySize operator *(BinarySize left, long right)
-    {
-        return left.Value * right;
-    }
-
-    /// <summary>
-    /// Implements the operator /.
-    /// </summary>
-    /// <param name="left">The left operand.</param>
-    /// <param name="right">The right operand.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static BinarySize operator /(BinarySize left, long right)
-    {
-        return left.Value / right;
-    }
-
-    /// <summary>
-    /// Implements the unary operator -.
-    /// </summary>
-    /// <param name="value">The operand.</param>
-    /// <returns>
-    /// The result of the operator.
-    /// </returns>
-    public static BinarySize operator -(BinarySize value)
-    {
-        return -value.Value;
-    }
-
-    #region Conversion operators
-
-    /// <summary>
-    /// Performs an explicit conversion from <see cref="Ookii.Jumbo.BinarySize"/> to <see cref="System.Int16"/>.
-    /// </summary>
-    /// <param name="value">The value.</param>
-    /// <returns>
-    /// The result of the conversion.
-    /// </returns>
-    public static explicit operator long(BinarySize value) => value.Value;
-
-    /// <summary>
-    /// Performs an implicit conversion from <see cref="System.Int64"/> to <see cref="Ookii.Jumbo.BinarySize"/>.
-    /// </summary>
-    /// <param name="value">The value.</param>
-    /// <returns>
-    /// The result of the conversion.
-    /// </returns>
-    public static implicit operator BinarySize(long value) => new BinarySize(value);
-
-    #endregion
 
 
     #region IEquatable<ByteSize> Members
