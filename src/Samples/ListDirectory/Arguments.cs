c46c0094dedea7f4dc48c6008d5e9efefcf381a9
Sven Groot <svgroot@microsoft.com>
2023-07-14 15:12:18 -0700

    Add ListDirectory sample.

---
+++ src/Samples/ListDirectory/Arguments.cs
@@ -0,0 +1,19 @@
+﻿using Ookii;
+using Ookii.CommandLine;
+using System.ComponentModel;
+
+namespace ListDirectory;
+
+[GeneratedParser]
+[ParseOptions(IsPosix = true)]
+[Description("Lists the files in a directory, including their sizes in a human-readable format.")]
+partial class Arguments
+{
+    [CommandLineArgument(IsPositional = true, IsShort = true, IncludeDefaultInUsageHelp = false)]
+    [Description("The path of the directory to list. The default value is the current working directory.")]
+    public DirectoryInfo Path { get; set; } = new DirectoryInfo(".");
+
+    [CommandLineArgument(IsShort = true)]
+    [Description("The minimum size of the files to include in the listing. This value can use suffixes such as 'KB' or 'MiB'.")]
+    public BinarySize? MinSize { get; set; }
+}
---
+++ src/Samples/ListDirectory/Program.cs
@@ -0,0 +1,25 @@
+﻿using ListDirectory;
+using Ookii;
+
+var arguments = Arguments.Parse();
+if (arguments == null)
+{
+    return 1;
+}
+
+var files = arguments.Path.EnumerateFiles().Select(f => new { f.Name, Size = (BinarySize)f.Length });
+if (arguments.MinSize is BinarySize minSize)
+{
+    files = files.Where(f => f.Size >= minSize);
+}
+
+foreach (var file in files)
+{
+    Console.WriteLine($"{file.Name}: {file.Size:0.# SiB}");
+}
+
+var total = files.Select(f => f.Size).Sum();
+Console.WriteLine();
+Console.WriteLine($"Total: {total:0.# SiB}");
+
+return 0;
